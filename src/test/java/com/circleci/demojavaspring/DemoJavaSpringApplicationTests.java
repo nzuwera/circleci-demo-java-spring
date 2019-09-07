ed9cc6c14a657c9fc69fcfe6c5a3b6dd9899ed21
nzuwera <nzuwera2002@gmail.com>
2019-09-07 07:10:43 +0200

    removed tests
    Removed unused test cases

--- src/test/java/com/circleci/demojavaspring/DemoJavaSpringApplicationTests.java
+++ src/test/java/com/circleci/demojavaspring/DemoJavaSpringApplicationTests.java
@@ -1,11 +1,14 @@
 package com.circleci.demojavaspring;
 
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import javax.validation.constraints.AssertTrue;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -15,7 +18,7 @@ public class DemoJavaSpringApplicationTests {
 
     @Test
     public void contextLoads() {
-
+        Assert.assertTrue(true);
     }
 
 }
